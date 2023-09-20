6ca770731b5cfe55a8a6c6d1c39e09d1df6edfcc
karalkal <karalkal@yahoo.co.uk>
2023-09-20 22:17:45 +0100

    task matrix - creating randomized 1-dimensional array

--- 06-matrix--find-your-hat/main.js
+++ 06-matrix--find-your-hat/main.js
@@ -20,16 +20,37 @@ class Field {
     static generateField(width, height, percentageHoles) {
         const totalArea = width * height
         const holesCount = Math.floor(percentageHoles * 100 / totalArea)       // percent of area (floor) 
-        const pathCount = totalArea - holesCount                               // minus current position and target positiin
+        const pathCount = totalArea - holesCount - 2                            // minus current position and target positiin
         console.log(holesCount, pathCount)
         const array = [hat, pathCharacter]
             .concat(new Array(holesCount).fill(hole))
             .concat(new Array(pathCount).fill(fieldCharacter))
 
-        console.log(array)
-        this.field = ["a", "b", "c", holesCount]
+
+        let randomizedArray = this.randomizeArray(array)
+        this.field = [randomizedArray]
+        
         console.log(this.field)
     }
+
+    static randomizeArray(array) {       // credit to https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+        let currentIndex = array.length, randomIndex;
+
+        // While there remain elements to shuffle.
+        while (currentIndex > 0) {
+
+            // Pick a remaining element.
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+        }
+
+        return array;
+    }
+
 }
 
 console.log('Your goal is to find your hat, marked with an ^.\nExit by pressing "c", landing on (and falling in) a hole \or moving "outside" the field.')
