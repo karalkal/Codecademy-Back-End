74c410c6dcd78c4430322bfcafb0b2be14535d3a
karalkal <karalkal@yahoo.co.uk>
2023-09-21 22:57:31 +0100

    moved some functions to utils module

--- 06-matrix--find-your-hat/main.js
+++ 06-matrix--find-your-hat/main.js
@@ -1,5 +1,8 @@
 const prompt = require('prompt-sync')({ sigint: true });
-var colors = require("colors/safe");
+const colors = require("colors/safe");
+
+const { displayIntro, getAndCheckInput, randomizeArray } = require('./util');
+
 
 const hatChar = colors.bgBlack('^');
 const holeChar = colors.bgBlack('O');
@@ -20,7 +23,8 @@ class Field {
 
     print() {
         for (let r of this.field) {
-            console.log(r.join(""), this.xPosition, this.yPosition)
+            console.log(r.join(""))
+            // console.log(r.join(""), this.xPosition, this.yPosition)
         }
     }
 
@@ -33,30 +37,17 @@ class Field {
             .concat(new Array(holesCount).fill(holeChar))
             .concat(new Array(accessibleCount).fill(fieldChar))
         // 2. shuffle it
-        let randomizedArray = this.randomizeArray(array)
+        let randomizedArray = randomizeArray(array)
         // 3. make it two dimensional and determine initial X and Y
         let initialData = this.createMatrixFindInitialPosition(randomizedArray, width, height)
         let matrix = initialData.matrix
         let xPosition = initialData.xInitial
         let yPosition = initialData.yInitial
-        // 4. update this        
+        // 4. update this (arg given to constructor)    
         return new this(matrix, xPosition, yPosition)
     }
 
-    static randomizeArray(initilArray) {       // credit to https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-        let currentIndex = initilArray.length, randomIndex;
-        // While there remain elements to shuffle.
-        while (currentIndex > 0) {
-            // Pick a remaining element.
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex--;
-            // And swap it with the current element.
-            [initilArray[currentIndex], initilArray[randomIndex]] = [
-                initilArray[randomIndex], initilArray[currentIndex]];
-        }
-        return initilArray;
-    }
-
+    
     static createMatrixFindInitialPosition(originalArray, width, height) {
         let matrix = []
         let xInitial = null
@@ -114,24 +105,11 @@ class Field {
     }
 }
 
+displayIntro();
 
-console.log(colors.blue('\nYour goal is to find your hat, marked with an ^.'))
-console.log(colors.blue('Avoid falling into the holes ("O") and moving outside the field.'))
-console.log(colors.blue('Exit by pressing "c"'))
-console.log(colors.blue('Use WASD to change position.\n'))
+try {
+    const { matrixWidth, matrixHeight, percentageHoles } = getAndCheckInput();
 
-let matrixWidth = Number(prompt(colors.green("Set width (8-26): ")))
-let matrixHeight = Number(prompt(colors.green("Set height 8-26): ")))
-let percentageHoles = Number(prompt(colors.green("Set hole percentage (8-26): ")))
-
-
-if (typeof matrixWidth === "NaN" || typeof matrixHeight === "NaN" || typeof percentageHoles === "NaN"
-    || matrixWidth < 8 || matrixWidth > 26
-    || matrixHeight < 8 || matrixHeight > 26
-    || percentageHoles < 8 || percentageHoles > 26) {
-    console.log(colors.red("\nAin't gonna work.\n"))
-    return
-} else {
     // newField.generateField(matrixWidth, matrixHeight, percentageHoles)   // static method, cannot call it with an instance
     let newMatrix = Field.generateField(matrixWidth, matrixHeight, percentageHoles)
     console.clear()
@@ -143,9 +121,9 @@ if (typeof matrixWidth === "NaN" || typeof matrixHeight === "NaN" || typeof perc
 
         let nextMove = prompt(colors.green("Next Move: "))
         console.clear()
-        if (nextMove === "c") {
+        if (nextMove === "c" || nextMove === "C") {
             console.clear()
-            console.log(colors.blue("Thanks, see ya later."))
+            console.log(colors.blue("Thanks, see ya later.\n"))
             break
         }
         if (!(["a", "s", "d", "w", "A", "S", "D", "W"].includes(nextMove))) {
@@ -163,6 +141,11 @@ if (typeof matrixWidth === "NaN" || typeof matrixHeight === "NaN" || typeof perc
 
     }
 }
+catch (error) {
+    console.log(colors.red("\n" + error.message + "\n"))
+}
+
+
 
 
 
---
+++ 06-matrix--find-your-hat/util.js
@@ -0,0 +1,44 @@
+const prompt = require('prompt-sync')({ sigint: true });
+const colors = require("colors/safe");
+
+
+function displayIntro() {
+    console.log(colors.blue('\nYour goal is to find your hat, marked with an ^.'))
+    console.log(colors.blue('Avoid falling into the holes ("O") and moving outside the field.'))
+    console.log(colors.blue('Exit by pressing "c"'))
+    console.log(colors.blue('Use WASD to change position.\n'))
+}
+
+
+function getAndCheckInput() {
+    let matrixWidth = Number(prompt(colors.green("Set width (8-26): ")))
+    let matrixHeight = Number(prompt(colors.green("Set height 8-26): ")))
+    let percentageHoles = Number(prompt(colors.green("Set hole percentage (8-26): ")))
+
+    if (typeof matrixWidth === "NaN" || typeof matrixHeight === "NaN" || typeof percentageHoles === "NaN"
+        || matrixWidth < 8 || matrixWidth > 26
+        || matrixHeight < 8 || matrixHeight > 26
+        || percentageHoles < 8 || percentageHoles > 26) {
+        throw new Error("Ain't gonna work like that.")
+    }
+    return { matrixWidth, matrixHeight, percentageHoles }
+}
+
+
+function randomizeArray(array) {       // credit to https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    let currentIndex = array.length, randomIndex;
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+    return array;
+}
+
+
+module.exports = { displayIntro, getAndCheckInput, randomizeArray }
+
