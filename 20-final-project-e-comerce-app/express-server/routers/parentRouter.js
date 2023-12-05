df724fb494d237a30bde085d98343418247f30c1
karalkal <karalkal@yahoo.co.uk>
2023-12-05 22:40:31 +0000

    created 'genre' routes, controller, query creators

--- 20-final-project-e-comerce-app/express-server/routers/parentRouter.js
+++ 20-final-project-e-comerce-app/express-server/routers/parentRouter.js
@@ -3,6 +3,7 @@ const express = require('express');
 const albumsRouter = require('./albumsRouter.js');
 const bandsRouter = require('./bandsRouter.js');
 const labelsRouter = require('./labelsRouter.js');
+const genresRouter = require('./genresRouter.js');
 
 const apiRouter = express.Router();
 
@@ -10,6 +11,7 @@ const apiRouter = express.Router();
 apiRouter.use("/bands", bandsRouter);
 apiRouter.use("/labels", labelsRouter);
 apiRouter.use("/albums", albumsRouter);
+apiRouter.use("/genres", genresRouter);
 
 
 module.exports = apiRouter;
